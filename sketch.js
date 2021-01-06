c985563ccb1aa6c6fb9b26ff3c0e869c93f1a154
sri vidya <74412969+paramesh486@users.noreply.github.com>
2021-01-06 05:41:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+const Engine=  Matter.Engine
+const World =  Matter.World
+const Bodies=  Matter.Bodies
+
+var engine, world;
+
+var x;
+var aditya=[];
+var singh=[]
+
+function setup() {
+  createCanvas(1200,600);
+engine=Engine.create();
+world= engine.world;
+
+x=6;
+console.log(x);
+
+aditya=["blue","yellow","cat","a"]
+          
+         
+singh=[["student1","aditya"],["student2","ram"],["student3","pavan"],["student4","sandeep"]]
+                
+
+
+for(var e=0      ; e<4      ;  e=e+1      ){
+console.log(singh[e][0]+"  "+singh[e][1])
+}
+
+
+}
+
+function draw() {
+  background("lightblue");  
+Engine.update(engine);
+
+
+
+
+}
