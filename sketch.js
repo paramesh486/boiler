757d211983d5e0ebd3e904bbed81bd62e80e42a7
sri vidya <74412969+paramesh486@users.noreply.github.com>
2021-03-01 10:48:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,28 +4,16 @@ const Bodies=  Matter.Bodies
 
 var engine, world;
 
-var x;
-var aditya=[];
-var singh=[]
 
 function setup() {
   createCanvas(1200,600);
 engine=Engine.create();
 world= engine.world;
 
-x=6;
-console.log(x);
 
-aditya=["blue","yellow","cat","a"]
-  
-         
-singh=[["student1","aditya"],["student2","ram"],["student3","pavan"],["student4","sandeep"]]
                 
 
 
-for(var e=0      ; e<4      ;  e=e+1      ){
-console.log(singh[e][0]+"  "+singh[e][1])
-}
 
 
 }
@@ -34,11 +22,9 @@ function draw() {
   background("lightblue");  
 Engine.update(engine);
 
-console.log(aditya);
-console.log(aditya[3])
 
 
-console.log("hai")
-console,log("bai")
+
+
 
 }
